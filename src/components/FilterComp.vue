2a003c0f45f44f581498861d06cb534541d6a59a
w-MAX-w <mxprojekt@gmail.com>
2019-09-10 17:37:19 +0300

    Выполнил задание 8

---
+++ src/components/FilterComp.vue
@@ -0,0 +1,20 @@
+<template>
+    <form action="#" class="search-form" @submit.prevent="$root.$children[0].$refs.products.filter(userSearch)">
+        <input type="text" class="search-field" v-model="userSearch">
+        <button class="btn-search" type="submit">
+            <i class="fas fa-search"></i>
+        </button>
+    </form>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            userSearch: ''
+        }
+    },
+
+    
+}
+</script>
